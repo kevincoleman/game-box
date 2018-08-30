f5d36ed43815b127cf8de6c5a9c9c42b39b82d62
kevincoleman <kevin.n.coleman@gmail.com>
2018-08-29 22:21:26 -0700

    Stub out a die class

--- scripts.js
+++ scripts.js
@@ -2,3 +2,22 @@ console.log("Welcome, pilgrim.");
 function alertMe() {
   alert ("I made a change!");
 }
+
+class Die {
+  constructor(numberOfSides) {
+    this.numberOfSides = numberOfSides;
+    this.sides = [];
+    for (let i = 0; i < numberOfSides; i++) {
+      this.sides.push(i + 1);
+    }
+  }
+
+  roll() {
+    return this.sides[Math.floor(Math.random()*this.sides.length)];
+  }
+  
+}
+
+let sixSided = new Die(6);
+console.log(sixSided);
+console.log(sixSided.roll());
