b2aa5afee52bf739ccacc1bb83a0afeb29ac7de4
kevincoleman <kevin.n.coleman@gmail.com>
2018-08-29 22:26:36 -0700

    Add helpful comments

--- scripts.js
+++ scripts.js
@@ -3,21 +3,25 @@ function alertMe() {
   alert ("I made a change!");
 }
 
+// A class for making dice
 class Die {
+  // A constructor is a function called when using the “new” keyword to create a new die object
   constructor(numberOfSides) {
     this.numberOfSides = numberOfSides;
+    // populate the die with the specified number of sides
     this.sides = [];
     for (let i = 0; i < numberOfSides; i++) {
       this.sides.push(i + 1);
     }
   }
 
+  // a function to roll the die
   roll() {
     return this.sides[Math.floor(Math.random()*this.sides.length)];
   }
-  
 }
 
+// testing out the 6-sided die
 let sixSided = new Die(6);
-console.log(sixSided);
-console.log(sixSided.roll());
+console.log("Here is a six-sided die:", sixSided);
+console.log("And we roll a", sixSided.roll());
